d8aca8b5df6526aa6f513645064657deeeb198c4
Muhammad Huzaifa <115563930+huzaifa21718@users.noreply.github.com>
2023-06-30 18:46:52 +0500

    Service Extensions method added

---
+++ Persistance/ServiceExtentions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance
+{
+    public static class ServiceExtentions
+    {
+        public static void AddPersistance(this IServiceCollection services)
+        {
+            //
+        }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,7 @@
+using Application;
+using Infrastructure;
+using Persistance;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+builder.Services.AddPersistance();
 
 var app = builder.Build();
 
