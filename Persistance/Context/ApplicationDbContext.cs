1d841e37abc4a904db443ce19044084081db1aac
Muhammad Huzaifa <115563930+huzaifa21718@users.noreply.github.com>
2023-08-07 06:09:55 +0500

    package removed

--- Persistance/Context/ApplicationDbContext.cs
+++ Persistance/Context/ApplicationDbContext.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Persistance.Context
 {
-    public class ApplicationDbContext : DbContext//, IApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
